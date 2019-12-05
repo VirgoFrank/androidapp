5405d1d8b8c514259db1d02805936f6a6c90cf4a
siimr <simkaviiner@gmail.com>
2019-12-05 09:37:46 +0200

    add layouts and activites

---
+++ App/App/Activities/AddActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App
+{
+    [Activity(Label = "AddActivity")]
+    public class AddActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.add_layout);
+
+            var titleField = FindViewById<EditText>(Resource.Id.titleText);
+            var descField = FindViewById<EditText>(Resource.Id.descText);
+            var saveButton = FindViewById<Button>(Resource.Id.saveButton);
+
+            saveButton.Click += async delegate
+            {
+                var title = titleField.Text;
+                var desc = descField.Text;
+                
+            };
+        }
+    }
+}
---
+++ App/App/Activities/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var searchEditText = FindViewById<EditText>(Resource.Id.searchEditText);
+            var searchButton = FindViewById<Button>(Resource.Id.searchButton);
+        
+            var toAddActivityButton = FindViewById<Button>(Resource.Id.addButton);
+
+            toAddActivityButton.Click += toAddActivityButton_Click;
+        }
+
+        private void toAddActivityButton_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(AddActivity));
+            StartActivity(intent);
+        }
+    }
+}
